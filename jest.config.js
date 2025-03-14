b92a5b52faaa36094d4e5fbdf4ebe6159b201e94
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2025-03-14 01:06:41 -0300

    Added test settings + aws dynamoDB lib

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testEnvironment: "node",
+  rootDir: "src/",
+  testMatch: ["**/*.test.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/"],
+  verbose: false,
+  preset: "ts-jest",
+  moduleDirectories: ["node_modules", "src"],
 };
