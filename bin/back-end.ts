f8ffb877b2beb465e5c3a6b18e5e1fbdeba321f6
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2025-03-15 00:10:35 -0300

    Added API Gateway to app starting point

--- bin/back-end.ts
+++ bin/back-end.ts
@@ -1,22 +1,32 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { DatabaseStack } from '../src/databaseModule/infrastructure';
-import { MainAppStack } from '../src/mainAppModule/infrastructure';
+import * as cdk from "aws-cdk-lib";
+import { DatabaseStack } from "../src/databaseModule/infrastructure";
+import { MainAppStack } from "../src/mainAppModule/infrastructure";
+import { ApiGatewayStack } from "../src/apiGatewayModule/infrasctructure";
 
 const app = new cdk.App();
 
-const databaseStack = new DatabaseStack(app, 'DatabaseStack', {
+const databaseStack = new DatabaseStack(app, "DatabaseStack", {
   tags: {
-    module: 'databaseModule'
+    module: "databaseModule",
   },
-  description: 'This stack creates the database resources for the application'
+  description: "This stack creates the database resources for the application",
 });
 
-const mainAppStack = new MainAppStack(app, 'MainAppStack', {
+const mainAppStack = new MainAppStack(app, "MainAppStack", {
   tags: {
-    module: 'mainAppModule'
+    module: "mainAppModule",
   },
-  description: 'This stack creates the main application resources for the application',
+  description: "This stack creates the main application resources",
   usersTable: databaseStack.usersTable,
 });
 mainAppStack.addDependency(databaseStack);
+
+const apiGatewayStack = new ApiGatewayStack(app, "ApiGatewayStack", {
+  tags: {
+    module: "apiGatewayModule",
+  },
+  description: "This stack creates the API Gateway resources",
+  createCustomerFunction: mainAppStack.createCustomerFunction,
+});
+apiGatewayStack.addDependency(mainAppStack);
