c4b02868bdbad05ac7c137578f6bac98a7d70977
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2025-03-15 00:09:48 -0300

    Added API Gateway definitions + created Add Customers api resources method

--- src/apiGatewayModule/infrasctructure/index.ts
+++ src/apiGatewayModule/infrasctructure/index.ts
@@ -4,18 +4,54 @@ import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cwLogs from "aws-cdk-lib/aws-logs";
 
+interface ApiGatewayStackProps extends cdk.StackProps {
+  createCustomerFunction: lambdaNodeJs.NodejsFunction;
+}
+
 export class ApiGatewayStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ApiGatewayStackProps) {
     super(scope, id, props);
 
-    const api = this.createApiGateway('MainAppApi');
+    const mainAppApi = this.createApiGateway("MainAppApi");
+    this.addCustomersResource(mainAppApi, props);
   }
 
   private createApiGateway(stackId: string): apigateway.RestApi {
+    const apiLogGroup = new cwLogs.LogGroup(this, `${stackId}LogGroup`);
     const api = new apigateway.RestApi(this, stackId, {
       restApiName: stackId,
+      description: `API Gateway for ${stackId}`,
+      cloudWatchRole: true,
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(
+          apiLogGroup
+        ),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true,
+        }),
+      },
     });
 
     return api;
+  }
+
+  private addCustomersResource(
+    api: apigateway.RestApi,
+    props: ApiGatewayStackProps
+  ): void {
+    // Create Customer resource
+    const createCustomerIntegration = new apigateway.LambdaIntegration(
+      props.createCustomerFunction
+    );
+    const customersResource = api.root.addResource("customers");
+    customersResource.addMethod("POST", createCustomerIntegration);
   }
 }
