bbccde05d29fbbaa7d491cca01ff868d4c433609
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2025-03-17 14:51:42 -0300

    Added root resource path

--- src/apiGatewayModule/infrasctructure/index.ts
+++ src/apiGatewayModule/infrasctructure/index.ts
@@ -1,8 +1,12 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as apiGateway from "aws-cdk-lib/aws-apigateway";
 import * as cwLogs from "aws-cdk-lib/aws-logs";
+// import * as route53 from "aws-cdk-lib/aws-route53";
+// import * as acm from "aws-cdk-lib/aws-certificatemanager";
+// import * as targets from "aws-cdk-lib/aws-route53-targets";
+// import { $config } from "../../../lib/config";
 
 interface ApiGatewayStackProps extends cdk.StackProps {
   createCustomerFunction: lambdaNodeJs.NodejsFunction;
@@ -13,20 +17,21 @@ export class ApiGatewayStack extends cdk.Stack {
     super(scope, id, props);
 
     const mainAppApi = this.createApiGateway("MainAppApi");
+    // this.addApiGatewayCustomDomain(mainAppApi, $config.STORE_API_GATEWAY_NAME); // custom domain set manually in the AWS Console and Cloudflare
     this.addCustomersResource(mainAppApi, props);
   }
 
-  private createApiGateway(stackId: string): apigateway.RestApi {
+  private createApiGateway(stackId: string): apiGateway.RestApi {
     const apiLogGroup = new cwLogs.LogGroup(this, `${stackId}LogGroup`);
-    const api = new apigateway.RestApi(this, stackId, {
+    const api = new apiGateway.RestApi(this, stackId, {
       restApiName: stackId,
       description: `API Gateway for ${stackId}`,
       cloudWatchRole: true,
       deployOptions: {
-        accessLogDestination: new apigateway.LogGroupLogDestination(
+        accessLogDestination: new apiGateway.LogGroupLogDestination(
           apiLogGroup
         ),
-        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+        accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
           httpMethod: true,
           ip: true,
           protocol: true,
@@ -38,20 +43,78 @@ export class ApiGatewayStack extends cdk.Stack {
           user: true,
         }),
       },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apiGateway.Cors.ALL_ORIGINS,
+        allowMethods: apiGateway.Cors.ALL_METHODS,
+        allowHeaders: apiGateway.Cors.DEFAULT_HEADERS,
+      },
     });
 
     return api;
   }
 
   private addCustomersResource(
-    api: apigateway.RestApi,
+    api: apiGateway.RestApi,
     props: ApiGatewayStackProps
   ): void {
+    // create API root resource
+    const apiRootResource = api.root.addResource("api");
+    
     // Create Customer resource
-    const createCustomerIntegration = new apigateway.LambdaIntegration(
+    const createCustomerIntegration = new apiGateway.LambdaIntegration(
       props.createCustomerFunction
     );
-    const customersResource = api.root.addResource("customers");
+
+    const customersResource = apiRootResource.addResource("customers");
     customersResource.addMethod("POST", createCustomerIntegration);
   }
+
+  // private addApiGatewayCustomDomain(
+  //   api: apiGateway.RestApi,
+  //   subdomain: string
+  // ): void {
+  //   // Created a hosted zone manually in the AWS Console for domain mashimo.dev.br
+  //   // so it just needs to be looked up
+  //   // if not created manually, create it programatically with "new route53.HostedZone(this, `${api.restApiName}GatewayHostedZone`, { zoneName: $config.DOMAIN_NAME });"
+  //   const hostedZone = route53.HostedZone.fromLookup(
+  //     this,
+  //     `${api.restApiName}GatewayHostedZone`,
+  //     {
+  //       domainName: $config.DOMAIN_NAME,
+  //     }
+  //   );
+
+  //   const certificate = new acm.Certificate(
+  //     this,
+  //     `${api.restApiName}GatewayDomainCertificate`,
+  //     {
+  //       domainName: subdomain,
+  //       validation: acm.CertificateValidation.fromDns(hostedZone),
+  //     }
+  //   );
+
+  //   new cdk.CfnOutput(this, `${api.restApiName}GatewayCertificateArn`, {
+  //     value: certificate.certificateArn,
+  //   });
+
+  //   api.addDomainName(`${api.restApiName}GatewayDomain`, {
+  //     domainName: subdomain,
+  //     certificate: certificate,
+  //   });
+
+    // Route 53 records are not required in this case, as DNS management is done in the domain registrar (Cloudflare)
+    // new route53.CnameRecord(this, `${api.restApiName}GatewayAliasRecord`, {
+    //   zone: hostedZone,
+    //   recordName: subdomain,
+    //   domainName: apiGatewayDomain.domainName,
+    // });
+
+    // new route53.ARecord(this, `${api.restApiName}GatewayAliasRecord`, {
+    //   zone: hostedZone,
+    //   recordName: subdomain,
+    //   target: route53.RecordTarget.fromAlias(
+    //     new targets.ApiGatewayDomain(apiGatewayDomain)
+    //   ),
+    // });
+  // }
 }
